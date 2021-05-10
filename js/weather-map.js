5856245ac46076229f893a4a6507595238710482
Javier Ruedas <javier@codeup.com>
2021-05-10 14:22:39 -0500

    get the get request working and test data output

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,14 +1,34 @@
-// $.get("https://api.openweathermap.org/data/2.5/weather", {
+// $.get("https://api.openweathermap.org/data/2.5/onecall", {
 //     APPID: OPEN_WEATHER_APPID,
 //     q:     "San Antonio, US"
 // }).done(function(data) {
 //     console.log(data);
 // });
-$.get("https://api.openweathermap.org/data/2.5/forecast", {
+
+// $.get("http://api.openweathermap.org/data/2.5/weather", {
+//     APPID: OPEN_WEATHER_APPID,
+//     lat:    29.423017,
+//     lon:   -98.48527,
+//     units: "imperial"
+// }).done(function(data) {
+//     console.log('current weather', data);
+// });
+
+// $.get("http://api.openweathermap.org/data/2.5/forecast", {
+//     APPID: OPEN_WEATHER_APPID,
+//     lat:    29.423017,
+//     lon:   -98.48527,
+//     units: "imperial"
+// }).done(function(data) {
+//     console.log('forecast', data);
+// });
+
+$.get("http://api.openweathermap.org/data/2.5/onecall", {
     APPID: OPEN_WEATHER_APPID,
     lat:    29.423017,
     lon:   -98.48527,
     units: "imperial"
 }).done(function(data) {
-    console.log('5 day forecast', data);
+    console.log('onecall', data);
+    console.log(data.lat);
 });
